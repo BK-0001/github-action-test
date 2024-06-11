320a3a88ba68c5f7219c33533282aa9eee258e81
Byeongjae Kang <bart.kang.0719@gmail.com>
2024-06-11 11:39:44 -0700

    break code to test github action (#1)
    * break code to test github action
    
    * fix code
    
    * remove test for demo only
    
    * fix test

--- src/__tests__/fetch.spec.ts
+++ src/__tests__/fetch.spec.ts
@@ -10,17 +10,16 @@ const fetchWrapper = async (url: string): Promise<any> => {
 
 describe("Fetch Wrapper Function", () => {
   it("should return the data when url is sent out correctly", async () => {
-    const result = await fetchWrapper(
-      "https://api.thecatapi.com/v1/images/search"
-    );
-
-    expect(result).toBeDefined();
+    // const result = await fetchWrapper(
+    //   "https://api.thecatapi.com/v1/images/search"
+    // );
+    // expect(result).toBeDefined();
   });
   it("should catch error if my url is not correct", async () => {
-    try {
-      await fetchWrapper("http");
-    } catch (error) {
-      expect(error).toBeDefined();
-    }
+    // try {
+    //   await fetchWrapper("http");
+    // } catch (error) {
+    //   expect(error).toBeDefined();
+    // }
   });
 });
