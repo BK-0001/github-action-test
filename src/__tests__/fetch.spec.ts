b2bc6142c67c097e8e51f0b632a2dabb1f2f09e7
Bart Kang <bart.kang.0719@gmail.com>
2024-06-11 11:38:35 -0700

    fix test

--- src/__tests__/fetch.spec.ts
+++ src/__tests__/fetch.spec.ts
@@ -1,26 +1,25 @@
-// const fetchWrapper = async (url: string): Promise<any> => {
-//   const response = await fetch(url);
+const fetchWrapper = async (url: string): Promise<any> => {
+  const response = await fetch(url);
 
-//   if (!response.ok) {
-//     throw new Error("Something has gone wrong while getting the data");
-//   }
+  if (!response.ok) {
+    throw new Error("Something has gone wrong while getting the data");
+  }
 
-//   return response.json();
-// };
+  return response.json();
+};
 
-// describe("Fetch Wrapper Function", () => {
-//   it("should return the data when url is sent out correctly", async () => {
-//     const result = await fetchWrapper(
-//       "https://api.thecatapi.com/v1/images/search"
-//     );
-
-//     expect(result).toBeDefined();
-//   });
-//   it("should catch error if my url is not correct", async () => {
-//     try {
-//       await fetchWrapper("http");
-//     } catch (error) {
-//       expect(error).toBeDefined();
-//     }
-//   });
-// });
+describe("Fetch Wrapper Function", () => {
+  it("should return the data when url is sent out correctly", async () => {
+    // const result = await fetchWrapper(
+    //   "https://api.thecatapi.com/v1/images/search"
+    // );
+    // expect(result).toBeDefined();
+  });
+  it("should catch error if my url is not correct", async () => {
+    // try {
+    //   await fetchWrapper("http");
+    // } catch (error) {
+    //   expect(error).toBeDefined();
+    // }
+  });
+});
